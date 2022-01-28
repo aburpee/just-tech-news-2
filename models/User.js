b14dd95a9269b3fdb19c7d682587839470e3bf76
bonobo <arburpee@gmail.com>
2022-01-27 18:42:43 -0800

    setting up db

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create our user model
+
+class User extends Model {}
+
+//define table columns and configuration
+
+User.init(
+    {
+        //table column definitions go here
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len:[4]
+            }
+        }
+    },
+    {
+        //table configuration options go here
+
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
